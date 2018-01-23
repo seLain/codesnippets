3f5928c9639b3dab57278ecc1e8ab50e52c95751
seLain <selain@nature.ee.ncku.edu.tw>
2018-01-23 12:47:03 +0800

    improvement with early modulo
    still, some test cases might cause timeout

--- python3/sam_and_substrings/main.py
+++ python3/sam_and_substrings/main.py
@@ -6,10 +6,11 @@ def substrings(balls):
     n = len(int_balls)
     total_sum = 0
     pow_sum = 0
+    mod_val = pow(10, 9)+7
     for i in range(n-1, -1, -1):
         pow_sum += pow(10, n-1-i)
-        total_sum += int_balls[i] * (i+1) * pow_sum
-    return total_sum % (pow(10, 9)+7)
+        total_sum += (int_balls[i] * (i+1) * pow_sum) % mod_val
+    return total_sum % mod_val
 
 if __name__ == "__main__":
     balls = input().strip()
